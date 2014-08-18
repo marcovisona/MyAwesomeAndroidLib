6ecce77b45a9e0d0a3b1dcb589969e31732014eb
Marco Visonà <marco@interno27.it>
2014-08-18 21:43:09 +0200

    initial commit

---
+++ src/main/java/com/example/mymodule/app2/TextOutput.java
@@ -0,0 +1,10 @@
+package com.example.mymodule.app2;
+
+/**
+ * Created by giorgia on 18/08/14.
+ */
+public class TextOutput {
+    public static String printText(){
+        return "Hello, text!";
+    }
+}
